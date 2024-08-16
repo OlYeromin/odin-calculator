3eb167df866668a16c96d4f554e8bd45e1dae035
OlYeromin <ol.yeromin@gmail.com>
2024-08-16 21:07:26 +0300

    Define the behavior of numeral buttons

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let firstNumber;
 let secondNumber;
 let operator;
+
+const numeralButtons = document.querySelectorAll("#numeral");
+numeralButtons.forEach((numeralButton) => {
+    numeralButton.addEventListener("click", () => {
+        const numeral = numeralButton.textContent;
+        if (operator === undefined) appendNumeral(numeral, firstNumber)
+            else appendNumeral(numeral, secondNumber);
+    })
+})
