c597a3c3245157c52ed98a3609fa461fb64bb840
OlYeromin <ol.yeromin@gmail.com>
2024-08-17 10:39:57 +0300

    Use array instead of string logic

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-let firstNumber = "";
-let secondNumber = "";
+let firstNumber = [];  // I tried strings first but they are immutable,
+let secondNumber = []; // i.e. you cannot change them unlike an array.
 let operator;
-
-function appendNumeral(numeralAppended, number) {
-    number.concat(numeralAppended); 
-    console.log(number);
-}
 
 const numeralButtons = document.querySelectorAll(".numeral");
 numeralButtons.forEach((numeralButton) => {
     numeralButton.addEventListener("click", () => {
         const numeral = numeralButton.textContent;
-        if (operator === undefined) appendNumeral(numeral, firstNumber)
-            else appendNumeral(numeral, secondNumber);
+        if (operator === undefined) firstNumber.push(Number(numeral))
+            else  secondNumber.push(Number(numeral));
     })
 })
