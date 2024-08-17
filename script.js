68ede5358d8b068572661bdbc98e9b6e09898bae
OlYeromin <ol.yeromin@gmail.com>
2024-08-17 10:42:06 +0300

    Bring back appendNumeral function
    I will need it for special cases (0 or a decimal in the beginning)

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let firstNumber = [];  // I tried strings first but they are immutable,
 let secondNumber = []; // i.e. you cannot change them unlike an array.
 let operator;
 
+function appendNumeral(numeralAppended, number) {
+    number.push(Number(numeralAppended)); 
+}
+
 const numeralButtons = document.querySelectorAll(".numeral");
 numeralButtons.forEach((numeralButton) => {
     numeralButton.addEventListener("click", () => {
         const numeral = numeralButton.textContent;
-        if (operator === undefined) firstNumber.push(Number(numeral))
-            else  secondNumber.push(Number(numeral));
+        if (operator === undefined) appendNumeral(numeral, firstNumber)
+            else appendNumeral(numeral, secondNumber);
     })
 })
