7809871f8c546b4a3e806e98d4d6d0857f33b240
OlYeromin <ol.yeromin@gmail.com>
2024-08-29 22:46:37 +0300

    Fix outputUp
    Before it pushed the result AND a second operator

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function outputUp(parameter) {
             currentDiv.textContent = outputContent;
             return;
         case "result":
-            currentDiv.textContent += `=${outputContent}`;
+            currentDiv.textContent += `=${result.join("")}`;
             return;
     }
 }
